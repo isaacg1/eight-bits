0fd127c8959c75b991badf5764e149f06aff4a06
Isaac Grosof <isaacbg227@gmail.com>
2023-03-20 13:33:43 -0400

    More solutions, more bells and whistles

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 #![feature(map_try_insert)]
-use std::collections::HashMap;
+use std::collections::{HashSet, HashMap};
 
 use integer_sqrt::IntegerSquareRoot;
 
-#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug)]
+#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug, PartialOrd, Ord)]
 struct Key {
     num: u16,
     zeros: u8,
     ones: u8,
 }
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialOrd, Ord, Eq, PartialEq)]
 enum Value {
     Direct(u8),
     Factorial(Key),
@@ -24,9 +24,17 @@ enum Value {
     Exp(Key, Key),
     Dominated,
 }
-fn search() {
+struct SearchParams {
+    limit: u16,
+    max_num: u16,
+    print_one: bool,
+    simple_print: bool,
+    print_big: bool,
+    skip_top: bool,
+    mark_max: bool,
+}
+fn search(params: SearchParams) {
     let mut table: HashMap<Key, Value> = HashMap::new();
-    let limit = 65535;
     let mut new_pairs = vec![];
     for i in 0..255u8 {
         let zeros = if i == 0 {
@@ -48,7 +56,7 @@ fn search() {
     while table_count != table.len() {
         table_count = table.len();
         for (key, value) in new_pairs.drain(..) {
-            if key.num > limit {
+            if key.num > params.limit {
                 continue;
             }
             let result = table.try_insert(key, value);
@@ -67,7 +75,10 @@ fn search() {
                 }
             }
         }
-        for (&key1, value1) in &table {
+        // Needed for consistent iteration order
+        let mut iter_pairs: Vec<(&Key, &Value)> = table.iter().collect();
+        iter_pairs.sort();
+        for (&key1, value1) in iter_pairs {
             assert!(key1.zeros <= 4);
             assert!(key1.ones <= 4);
             if matches!(value1, Value::Dominated) {
@@ -160,24 +171,55 @@ fn search() {
             }
         }
     }
-    'outer: for num in 0..=500 {
+    let mut max_len = 0;
+    'outer: for num in 0..=params.max_num {
+        let mut found = false;
+        let mut seen_top = HashSet::new();
         for sum in 0..=8u8 {
             for zeros in (sum.saturating_sub(4)..=4).rev() {
                 let ones = sum - zeros;
                 let key = Key { num, zeros, ones };
                 if let Some(value) = table.get(&key) {
                     if !matches!(value, Value::Dominated) {
-                        println!(
-                            "{}: {}",
-                            key.num,
-                            value_to_string(*value, &table, false)
-                        );
-                        continue 'outer
+                        if params.simple_print {
+                            println!("{}: {}", key.num, value_to_string(*value, &table, false));
+                        } else {
+                            if !params.print_big || big_num(*value) > params.max_num {
+                                let top_str = value_to_top(*value);
+                                let was_new = seen_top.insert(top_str);
+                                if !params.skip_top || was_new {
+                                    let string = value_to_string(*value, &table, false);
+                                    let star = if params.mark_max && string.len() > max_len {
+                                        max_len = string.len();
+                                        " *"
+                                    } else {
+                                        ""
+                                    };
+                                    println!(
+                                        "{}; {},{}: {:<9} {}{}",
+                                        key.num,
+                                        key.zeros,
+                                        key.ones,
+                                        value_to_top(*value),
+                                        value_to_string(*value, &table, false),
+                                        star
+                                    );
+                                }
+                            }
+                        }
+                        found = true;
+                        if params.print_one {
+                            continue 'outer;
+                        } else {
+                            break
+                        }
                     }
                 }
             }
         }
-        println!("{} was not found", num);
+        if !found && !params.print_big {
+            println!("{} was not found", num);
+        }
     }
 }
 fn value_to_string(value: Value, table: &HashMap<Key, Value>, wrap: bool) -> String {
@@ -244,6 +286,67 @@ fn value_to_string(value: Value, table: &HashMap<Key, Value>, wrap: bool) -> Str
         unwrapped_string
     }
 }
+fn value_to_top(value: Value) -> String {
+    match value {
+        Value::Direct(num) => num.to_string(),
+        Value::Plus(key1, key2) => format!("{}+{}", key1.num, key2.num),
+        Value::Minus(key1, key2) => format!("{}-{}", key1.num, key2.num),
+        Value::Times(key1, key2) => format!("{}*{}", key1.num, key2.num),
+        Value::Div(key1, key2) => format!("{}/{}", key1.num, key2.num),
+        Value::Exp(key1, key2) => format!("{}^{}", key1.num, key2.num),
+        Value::Factorial(key1) => format!("{}!", key1.num),
+        Value::DoubleFactorial(key1) => format!("{}!!", key1.num),
+        Value::Sqrt(key1) => format!("s({})", key1.num),
+        Value::TimesShift(key1, key2, shift) | Value::DivShift(key1, key2, shift) => {
+            let str1 = key1.num.to_string();
+            let str2 = format!("{:b}", key2.num);
+            let shift = shift as usize;
+            let op = match value {
+                Value::TimesShift(_, _, _) => '*',
+                Value::DivShift(_, _, _) => '/',
+                _ => unreachable!(),
+            };
+            if str2.len() > shift {
+                format!(
+                    "{str1}{op}{}.{}",
+                    &str2[..str2.len() - shift],
+                    &str2[str2.len() - shift..]
+                )
+            } else {
+                format!(
+                    "{str1}{op}.{}{str2}",
+                    (0..shift - str2.len()).map(|_| '0').collect::<String>()
+                )
+            }
+        }
+        _ => unimplemented!("{:?}", value),
+    }
+}
+fn big_num(value: Value) -> u16 {
+    match value {
+        Value::Direct(num) => num.into(),
+        Value::Factorial(key1) |
+        Value::DoubleFactorial(key1) |
+        Value::Sqrt(key1) => key1.num,
+        Value::Plus(key1, key2) |
+        Value::Minus(key1, key2) |
+        Value::Times(key1, key2) |
+        Value::Div(key1, key2) |
+        Value::Exp(key1, key2) => key1.num.max(key2.num),
+        Value::TimesShift(key1, _key2, _shift) | Value::DivShift(key1, _key2, _shift) => {
+            key1.num
+        }
+        _ => unimplemented!("{:?}", value),
+    }
+}
 fn main() {
-    search()
+    search(SearchParams {
+        limit: 65535,
+        max_num: 1000,
+        print_one: true,
+        simple_print: true,
+        print_big: false,
+        skip_top: false,
+        mark_max: false,
+    })
 }
